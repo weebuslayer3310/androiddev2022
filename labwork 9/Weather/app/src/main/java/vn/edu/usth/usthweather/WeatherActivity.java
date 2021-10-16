3c994b1301712eee4be8858d7b07c21f55bf8337
Nghia <abc@gmail.com>
2021-10-16 14:36:46 +0700

    added labwork 9

---
+++ labwork 9/Weather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,97 @@
+package vn.edu.usth.usthweather;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.Menu;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.header);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Weather", "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Weather", "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Weather", "onDestroy() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Weather", "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Weather", "onResume() called");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        Log.i("Weather", "onCreate() called");
+    }
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[] { "Hanoi", "Paris", "NewYork" };
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT; // number of pages for a ViewPager
+        }
+        @Override
+        public Fragment getItem(int page) {
+// returns an instance of Fragment corresponding to the specified page
+            switch (page) {
+                case 0: return new WeatherAndForecastFragment();
+                case 1: return new WeatherAndForecastFragment();
+                case 2: return new WeatherAndForecastFragment();
+            }
+            return new Fragment(); // failsafe
+        }
+        @Override
+        public CharSequence getPageTitle(int page) {
+// returns a tab title corresponding to the specified page
+            return titles[page];
+        }
+    }
+}
